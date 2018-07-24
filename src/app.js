42bd559b19324322cccbb3aaa1b91892a2f8d72c
Elizalde <javelisol@gmail.com>
2018-07-24 17:20:19 -0500

    the webpack is working, still needs the mode to be set in the configuration

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import {} from './app.css';
+import template from './index.html';
+import YOPFForm from './form/form.index';
+import YOPFFireworks from './fireworks/fireworks.index';
+
+(function() {
+    document.body.innerHTML = template;
+})();
